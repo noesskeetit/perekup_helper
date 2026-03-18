446243bc4e6de871209636fa9241d829c11819c5
Aleksandr Gabbasov <shura.gabbasov@mail.ru>
2026-03-18 19:19:02 +0300

    fix: chain alembic migrations to resolve multiple heads

--- alembic/versions/c3d4e5f6a7b8_add_deduplication_columns.py
+++ alembic/versions/c3d4e5f6a7b8_add_deduplication_columns.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "c3d4e5f6a7b8"
-down_revision: Union[str, Sequence[str], None] = "a1b2c3d4e5f6"
+down_revision: Union[str, Sequence[str], None] = "c1d2e3f4a5b6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
